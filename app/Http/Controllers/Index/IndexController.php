d73eda12ad87a15f927744166b3eca800a8bfe37
zhaotingquan <861970869@qq.com>
2020-08-26 09:26:40 +0800

    将jq和17素材爬取分开两个方法

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -11,15 +11,169 @@ namespace App\Http\Controllers\Index;
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Types\Self_;
 
 class IndexController extends Controller
 {
+    public function index00()
+    {
+        $url = "http://www.jq22.com/demo/treeSelect201905170004/ztree/img/";
+        self::file_exists_S3($url, 'z_4.png', 'D:\phpStudy1\PHPTutorial\WWW\get17sucai-master\public\moban\\');
+        return view("index.form")->with("");
+    }
+
     /**
-     * 实例
+     * 实例-爬取17素材
      * @param Request $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index(Request $request)
+    {
+        ini_set('max_execution_time', 600);//秒为单位，自己根据需要定义
+        if ($request->isMethod("post")) {
+            try {
+                $client = new Client();
+                //"https://www.17sucai.com/preview/806169/2020-03-11/车辆大数据展示平台/index.html";
+                $url = str_replace("index.html", "", $request->post('url'));
+                //判断是否有参数
+                if (empty($url)) {
+                    echo "<html><head><style>html,body{padding: 0;margin: 0;width: 100%;height: 100%;}body{display: flex;align-items: center;justify-content: center;}</style></head><body><div class=\"div\"><a href='javascript:history.back(-1)'>没有输入正确网址，点击返回上一页</a></div></body></html>";
+                    exit;
+                }
+                //获取index内容
+                $response = $client->get($url . 'index.html', [
+                    "verify" => false
+                ]);
+                $html = $response->getBody()->getContents();
+                //判断是否有内容
+                if (empty($html)) {
+                    echo "<html><head><style>html,body{padding: 0;margin: 0;width: 100%;height: 100%;}body{display: flex;align-items: center;justify-content: center;}</style></head><body><div class=\"div\"><a href='javascript:history.back(-1)'>没有获取到网页内容，点击返回上一页</a></div></body></html>";
+                    exit;
+                }
+                //创建文件夹
+                $filepath = public_path() . "\\moban\\" . date("YmdH" . str_pad(mt_rand(0, 9999), 4, "0", STR_PAD_LEFT), time()) . "\\";
+                !is_dir($filepath) ? mkdir($filepath, 0777, true) : null;
+                $myfile = fopen($filepath . "index.html", "w") or die("Unable to open file!");
+                fwrite($myfile, $html);
+                fclose($myfile);
+                $pattern_js = '/<script[^>]*?src="(.*?)"[^>]*?><\/script>/si';
+                $pattern_js_ext = '/<script[^>]*?src=\'(.*?)\'[^>]*?><\/script>/si';
+                $pattern_css = '/<link[^>]*?href="(.*?)"[^>]*?>/si';
+                $pattern_css_ext = '/<link[^>]*?href=\'(.*?)\'[^>]*?>/si';
+                $pattern_img = '/<img[^>]*?src="(.*?)"[^>]*?>/si';
+                $pattern_img_ext = '/<img[^>]*?src=\'(.*?)\'[^>]*?>/si';
+                $css_src = '/src:[^>]*?url\((.*?)\)/si';
+                $css_background = '/background:[^>]*?url\((.*?)\)/si';
+                $css_background_image = '/background-image:[^>]*?url\((.*?)\)/si';
+                preg_match_all($pattern_js, $html, $match_js);
+                preg_match_all($pattern_js_ext, $html, $match_js_ext);
+                $match_js_result = array_merge($match_js[1] ?? array(), $match_js_ext[1] ?? array());
+                preg_match_all($pattern_css, $html, $match_css);
+                preg_match_all($pattern_css_ext, $html, $match_css_ext);
+                $match_css_result = array_merge($match_css[1] ?? array(), $match_css_ext[1] ?? array());
+                preg_match_all($pattern_img, $html, $match_img);
+                preg_match_all($pattern_img_ext, $html, $match_img_ext);
+                $match_img_result = array_merge($match_img[1] ?? array(), $match_img_ext[1] ?? array());
+                //匹配css文件
+                if (isset($match_css_result)) {
+                    foreach ($match_css_result as $k => $v) {
+                        if (strstr($v, "http://") || strstr($v, "https://")) continue;
+                        //
+                        if (isset(get_headers($url . $v, true)[0]) && (strpos(get_headers($url . $v, true)[0], '200') || strpos(get_headers($url . $v, true)[0], '304'))) {
+                            $content_css = file_get_contents($url . $v);
+                        } else {
+                            $content_css = "";
+                        }
+                        //处理css中的文件
+                        preg_match_all($css_src, $content_css, $css_src_result);
+                        preg_match_all($css_background, $content_css, $css_background_result);
+                        preg_match_all($css_background_image, $content_css, $css_background_image_result);
+                        //获取css文件中的src引用文件
+                        if (isset($css_src_result[1])) {
+                            foreach ($css_src_result[1] as $kk => $vv) {
+                                $cache_string = str_replace("'", "", $vv);
+                                $cache_string = str_replace("\"", "", $cache_string);
+                                $cache_string = str_replace("../", '', $cache_string);
+                                if (isset(get_headers($url . $cache_string, true)[0]) && (strpos(get_headers($url . $cache_string, true)[0], '200') || strpos(get_headers($url . $cache_string, true)[0], '304'))) {
+                                    self::file_exists_S3($url, $cache_string, $filepath);
+                                }
+                            }
+                        }
+                        //获取background引用的图片文件
+                        if (isset($css_background_result[1])) {
+                            foreach ($css_background_result[1] as $kk => $vv) {
+                                $cache_string = str_replace("'", "", $vv);
+                                $cache_string = str_replace("\"", "", $cache_string);
+                                $cache_string = str_replace("../", '', $cache_string);
+                                if (isset(get_headers($url . $cache_string, true)[0]) && (strpos(get_headers($url . $cache_string, true)[0], '200') || strpos(get_headers($url . $cache_string, true)[0], '304'))) {
+                                    self::file_exists_S3($url, $cache_string, $filepath);
+                                }
+                            }
+                        }
+                        //获取background-image引用的图片文件
+                        if (isset($css_background_image_result[1])) {
+                            foreach ($css_background_image_result[1] as $kk => $vv) {
+                                $cache_string = str_replace("'", "", $vv);
+                                $cache_string = str_replace("\"", "", $cache_string);
+                                $cache_string = str_replace("../", '', $cache_string);
+                                if (isset(get_headers($url . $cache_string, true)[0]) && (strpos(get_headers($url . $cache_string, true)[0], '200') || strpos(get_headers($url . $cache_string, true)[0], '304'))) {
+                                    self::file_exists_S3($url, $cache_string, $filepath);
+                                }
+                            }
+                        }
+                        $filename_array = explode("/", $v);
+                        $count = count($filename_array);
+                        $filecsspath = $filepath;
+                        for ($i = 0; $i < $count - 1; $i++) {
+                            $filecsspath .= $filename_array[$i] . "\\";
+                        }
+                        !is_dir($filecsspath) ? mkdir($filecsspath, 0777, true) : null;
+                        $myfile = fopen($filecsspath . $filename_array[$count - 1], "w") or die("Unable to open file!");
+                        fwrite($myfile, $content_css);
+                        fclose($myfile);
+                    }
+                }
+                //匹配JS文件
+                if (isset($match_js_result)) {
+                    foreach ($match_js_result as $k => $v) {
+                        if (strstr($v, "http://") || strstr($v, "https://")) continue;
+                        if (isset(get_headers($url . $v, true)[0]) && (strpos(get_headers($url . $v, true)[0], '200') || strpos(get_headers($url . $v, true)[0], '304'))) {
+                            $content_js = file_get_contents($url . $v);
+                        } else {
+                            $content_js = "";
+                        }
+                        $filename_array = explode("/", $v);
+                        $count = count($filename_array);
+                        $filejspath = $filepath;
+                        for ($i = 0; $i < $count - 1; $i++) {
+                            $filejspath .= $filename_array[$i] . "\\";
+                        }
+                        !is_dir($filejspath) ? mkdir($filejspath, 0777, true) : null;
+                        $myfile = fopen($filejspath . $filename_array[$count - 1], "w") or die("Unable to open file!");
+                        fwrite($myfile, $content_js);
+                        fclose($myfile);
+                    }
+                }
+                //匹配img文件
+                if (isset($match_img_result)) {
+                    foreach ($match_img_result as $k => $v) {
+                        if (strstr($v, "http://") || strstr($v, "https://")) continue;
+                        self::file_exists_S3($url, $v, $filepath);
+                    }
+                }
+                echo "<html><head><style>html,body{padding: 0;margin: 0;width: 100%;height: 100%;}body{display: flex;align-items: center;justify-content: center;}</style></head><body><div class=\"div\"><script type=\"text/javascript\">function copyUrl2(){var Url2=document.getElementById('content');Url2.select();document.execCommand('Copy');alert('已复制好，可贴粘。');}</script><textarea id=\"content\">" . $filepath . "</textarea><br /><input type=\"button\" onClick=\"copyUrl2()\" value=\"点击复制代码\" /><br><br><a href='javascript:history.back(-1)'>继续爬取，点击返回上一页</a></div></body></html>";
+                exit;
+            } catch (\Exception $exception) {
+                $result["code"] = $exception->getCode();
+                $result['msg'] = $exception->getMessage();
+                echo json_encode($result);
+                exit;
+            }
+        }
+        return view("index.form")->with("");
+    }
+    public function index1(Request $request)
     {
         ini_set('max_execution_time', 600);//秒为单位，自己根据需要定义
         if ($request->isMethod("post")) {
@@ -77,6 +231,20 @@ class IndexController extends Controller
                         } else {
                             $content_css = "";
                         }
+
+                        $filename_array = explode("/", $v);
+                        $count = count($filename_array);
+                        $filecsspath = $filepath;
+                        $flag = 0;
+                        for ($i = 0; $i < $count - 1; $i++) {
+                            $filecsspath .= $filename_array[$i] . "\\";
+                            $flag = $i;
+                        }
+                        !is_dir($filecsspath) ? mkdir($filecsspath, 0777, true) : null;
+                        $myfile = fopen($filecsspath . $filename_array[$count - 1], "w") or die("Unable to open file!");
+                        fwrite($myfile, $content_css);
+                        fclose($myfile);
+
                         //处理css中的文件
                         preg_match_all($css_src, $content_css, $css_src_result);
                         preg_match_all($css_background, $content_css, $css_background_result);
@@ -86,7 +254,7 @@ class IndexController extends Controller
                             foreach ($css_src_result[1] as $kk => $vv) {
                                 $cache_string = str_replace("'", "", $vv);
                                 $cache_string = str_replace("\"", "", $cache_string);
-                                $cache_string = str_replace("../", "", $cache_string);
+                                if ($flag >= 1)$cache_string = str_replace("../", $filename_array[$flag-1] . '/', $cache_string);
                                 if (isset(get_headers($url . $cache_string, true)[0]) && (strpos(get_headers($url . $cache_string, true)[0], '200') || strpos(get_headers($url . $cache_string, true)[0], '304'))) {
                                     self::file_exists_S3($url, $cache_string, $filepath);
                                 }
@@ -97,7 +265,7 @@ class IndexController extends Controller
                             foreach ($css_background_result[1] as $kk => $vv) {
                                 $cache_string = str_replace("'", "", $vv);
                                 $cache_string = str_replace("\"", "", $cache_string);
-                                $cache_string = str_replace("../", "", $cache_string);
+                                if ($flag >= 1)$cache_string = str_replace("../", $filename_array[$flag-1] . '/', $cache_string);
                                 if (isset(get_headers($url . $cache_string, true)[0]) && (strpos(get_headers($url . $cache_string, true)[0], '200') || strpos(get_headers($url . $cache_string, true)[0], '304'))) {
                                     self::file_exists_S3($url, $cache_string, $filepath);
                                 }
@@ -108,22 +276,12 @@ class IndexController extends Controller
                             foreach ($css_background_image_result[1] as $kk => $vv) {
                                 $cache_string = str_replace("'", "", $vv);
                                 $cache_string = str_replace("\"", "", $cache_string);
-                                $cache_string = str_replace("../", "", $cache_string);
+                                if ($flag >= 1)$cache_string = str_replace("../", $filename_array[$flag-1]. '/', $cache_string);
                                 if (isset(get_headers($url . $cache_string, true)[0]) && (strpos(get_headers($url . $cache_string, true)[0], '200') || strpos(get_headers($url . $cache_string, true)[0], '304'))) {
                                     self::file_exists_S3($url, $cache_string, $filepath);
                                 }
                             }
                         }
-                        $filename_array = explode("/", $v);
-                        $count = count($filename_array);
-                        $filecsspath = $filepath;
-                        for ($i = 0; $i < $count - 1; $i++) {
-                            $filecsspath .= $filename_array[$i] . "\\";
-                        }
-                        !is_dir($filecsspath) ? mkdir($filecsspath, 0777, true) : null;
-                        $myfile = fopen($filecsspath . $filename_array[$count - 1], "w") or die("Unable to open file!");
-                        fwrite($myfile, $content_css);
-                        fclose($myfile);
                     }
                 }
                 //匹配JS文件
@@ -163,11 +321,20 @@ class IndexController extends Controller
                 exit;
             }
         }
-        return view("index.form")->with("");
+        return view("index1.form")->with("");
     }
 
-    function file_exists_S3($url,$filename, $filepath)
+    /**
+     * https://blog.csdn.net/muzi187/article/details/56486178?utm_source=blogxgwz5
+     * https://www.php.cn/php-weizijiaocheng-394566.html
+     * @param $url
+     * @param $filename
+     * @param $filepath
+     * @return int
+     */
+    function file_exists_S3($url, $filename, $filepath)
     {
+        file_put_contents('D:\phpStudy1\PHPTutorial\WWW\get17sucai-master\public\moban\log.txt', $url . '---' . $filename . '---' . $filepath . PHP_EOL, FILE_APPEND);
         $state = @file_get_contents($url . $filename, 0, null, 0, 1);//获取网络资源的字符内容
         if ($state) {
             $filename_array = explode("/", $filename);
---
+++ resources/views/index1/form.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<meta http-equiv="Cache-Control" content="no-siteapp">
+		<title>简洁搜索 - 极简多引擎主页</title>
+		<meta name="author" content="ZhangDi" />
+		<meta name="description" content="极简多引擎主页，提供聚合搜索框和网址导航。" />
+		<meta name="keywords" content="简洁搜索,极简搜索,简洁导航" />
+		<link rel="dns-prefetch" href="{{ URL::asset('/') }}" />
+		<link rel="icon" href="{{ URL::asset('search/img/search.png') }}" sizes="192x192" />
+		<link rel="apple-touch-icon-precomposed" href="{{ URL::asset('search/img/search.png') }}" />
+		<meta name="msapplication-TileImage" content="{{ URL::asset('search/img/search.png') }}" />
+		<link rel="shortcut icon" href="{{ URL::asset('search/img/search.png') }}" />
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-touch-fullscreen" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta name="full-screen" content="yes">
+		<!--UC强制全屏-->
+		<meta name="browsermode" content="application">
+		<!--UC应用模式-->
+		<meta name="x5-fullscreen" content="true">
+		<!--QQ强制全屏-->
+		<meta name="x5-page-mode" content="app">
+		<!--CSRF-->
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+		<!--QQ应用模式-->
+		<link href="{{ URL::asset('search/font/embed.css') }}" rel="stylesheet">
+		<link href="{{ URL::asset('search/css/style.min.css') }}" rel="stylesheet">
+		<script src="{{ URL::asset('search/t/font_1592760_frznr49dvo.js') }}"></script>
+		<script src="{{ URL::asset('search/t/font_1592665_8irfenx66gf.js') }}"></script>
+		<script src="{{ URL::asset('search/js/jquery.min.js') }}"></script>
+		<script src="{{ URL::asset('search/js/sou.min.js') }}"></script>
+		<link rel="shortcut icon" href="{{ URL::asset('search/img/logo.ico') }}" />
+		<link rel="stylesheet" href="{{ URL::asset('search/css/search.min.css') }}" />
+		<!-- 百度统计 -->
+		<script>
+			// console.log('%chttp://www.yunziyuan.com.cn/','color: yellow; background: red; font-size: 24px;font-size:100px')
+      </script>
+
+	</head>
+	<body>
+{{--		<div id="menu"><i></i></div>--}}
+{{--		<div class="list closed">--}}
+{{--			<ul>--}}
+{{--			</ul>--}}
+{{--		</div>--}}
+		<div id="content">
+			<div class="con">
+				<div class="shlogo">
+					<div class="logo-text embed">简洁&nbsp;&nbsp;搜索</div>
+				</div>
+				<section class="sousuo">
+					<div class="search">
+						<div class="search-box">
+{{--							<svg class="icon" id="search-icon" aria-hidden="true">--}}
+{{--								<use xlink:href="#icon-baidu"></use>--}}
+{{--							</svg>--}}
+							<form action="{{ url('index1') }}" method="post" style="width: 100%">
+								{{ csrf_field() }}
+								<input type="text" id="" class="search-input" autocomplete="off" autofocus="autofocus" placeholder="请出入页面网址" name="url" style="width: 100%"/>
+							</form>
+{{--							<svg class="icon" aria-hidden="true" id="search-clear" style="display: none;">--}}
+{{--								<use xlink:href="#icon-shanchu"></use>--}}
+{{--							</svg>--}}
+						</div>
+						<!-- 搜索热词 -->
+						<div class="box search-hot-text" id="box" style="display: none;">
+							<ul></ul>
+						</div>
+						<!-- 搜索引擎 -->
+						<div class="search-engine" style="display: none;">
+							<div class="search-engine-head">
+								<strong class="search-engine-tit">选择您的默认搜索引擎：</strong>
+								<div class="search-engine-tool">
+									搜索热词：<label class="switch">
+										<input id="hot-btn" type="checkbox">
+										<div class="slider round"></div>
+									</label>
+								</div>
+							</div>
+							<ul class="search-engine-list"></ul>
+						</div>
+					</div>
+				</section>
+			</div>
+		</div>
+	</body>
+	<script src=" {{ URL::asset('search/js/search.min.js') }}"> </script>
+	<script src=" {{ URL::asset('search/js/jsonload.js') }}"> </script>
+</html>
